767166c82d94bfbb1b965cc77d091f2675be077b
BerilKose <berl.3225@gmail.com>
2026-03-12 23:30:21 +0300

    Final version

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from custom_classes import Calculator
 value = Calculator()
 
-print(value._current_val) #printing the current value from the Calculator class
+#print(value._current_val) #printing the current value from the Calculator class
+
+#testing the merged branches
+result = value.add(10,5) * 2 / 3
+print(result)
+
