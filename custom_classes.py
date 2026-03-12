f819c81bb49c7817a3b3ccf9ed6536e812a829cc
BerilKose <berl.3225@gmail.com>
2026-03-12 23:09:57 +0300

    Add divide() method

--- custom_classes.py
+++ custom_classes.py
@@ -4,5 +4,7 @@ class Calculator:
 
         
     def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero")
         self._current_val = x / y
         return self._current_val
